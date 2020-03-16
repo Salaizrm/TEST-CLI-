cb5aed209d57e0bf8cb726822f2f5b4627d1a8bf
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-16 01:46:06 +0000

    first commit

---
+++ TEST-CLI-/lib/TEST/CLI/.rb
@@ -0,0 +1,8 @@
+require "TEST/CLI//version"
+
+module TEST
+  module CLI
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ TEST-CLI-/lib/TEST/CLI/version.rb
@@ -0,0 +1,5 @@
+module TEST
+  module CLI
+    VERSION = "0.1.0"
+  end
+end
